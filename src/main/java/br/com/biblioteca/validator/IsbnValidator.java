b64a625c353067fde03f9ab81e5996dfde9dd587
Isaque Cunha <costaisaque716@gmail.com>
2026-07-23 21:40:02 -0300

    feat: adicionado validação de ISBN-10

--- src/main/java/br/com/biblioteca/validator/IsbnValidator.java
+++ src/main/java/br/com/biblioteca/validator/IsbnValidator.java
@@ -3,8 +3,8 @@ package br.com.biblioteca.validator;
 import br.com.biblioteca.exception.IsbnInvalidoException;
 
 public final class IsbnValidator {
-
-    private static final int TAMANHO_ISBN = 13;
+    private static final int TAMANHO_ISBN10 = 10;
+    private static final int TAMANHO_ISBN13 = 13;
 
     private IsbnValidator() {
 
@@ -13,15 +13,20 @@ public final class IsbnValidator {
     public static String validar(String isbn) {
         String limpo = limpar(isbn);
 
-        if (limpo.length() != TAMANHO_ISBN || limpo.matches("\\d+")) {
-            throw new IsbnInvalidoException(isbn);
+        if (limpo.length() == TAMANHO_ISBN10) {
+            if (!isbn10Valido(limpo)){
+                throw new IsbnInvalidoException(isbn);
+            }
+            return limpo.substring(0,9) + Character.toUpperCase(limpo.charAt(9));
         }
 
-        if (!digitoVerificadorValido(limpo)){
-            throw new IsbnInvalidoException(isbn);
+        if (limpo.length() == TAMANHO_ISBN13) {
+            if (!isbn13Valido(limpo)){
+                throw new IsbnInvalidoException(isbn);
+            }
+            return limpo;
         }
-
-        return limpo;
+        throw new IsbnInvalidoException(isbn);
     }
 
     private static String limpar(String isbn) {
@@ -29,25 +34,39 @@ public final class IsbnValidator {
             return "";
         }
 
-        return isbn.replaceAll("\\D", "");
+        return isbn.trim().replaceAll("[^0-9Xx]", "");
     }
 
-    private static boolean digitoVerificadorValido(String isbn) {
+    private static boolean isbn10Valido(String isbn) {
         int soma = 0;
-
-        for (int i = 0; i<12; i++){
-            int digito = isbn.charAt(i) - '0';
-
-            if (i%2 == 0){
-                soma += digito;
-            }else {
-                soma += digito*3;
+        for (int i = 0; i < 9; i++){
+            char c = isbn.charAt(i);
+            if (!Character.isDigit(c)){
+                return false;
             }
+            soma += (c - '0') * (10 - i);
         }
-        int resto = soma % 10;
-        int digitoEsperado = (resto == 0) ? 0:10 - resto;
 
-        int digitoInformado = isbn.charAt(12) -  '0';
-        return digitoEsperado == digitoInformado;
+        char ultimo = Character.toUpperCase(isbn.charAt(9));
+        if (ultimo == 'X'){
+            soma += 10;
+        } else if (Character.isDigit(ultimo)) {
+            soma += (ultimo - '0');
+        }else {
+            return false;
+        }
+        return soma % 11 == 0;
+    }
+
+    private static boolean isbn13Valido(String isbn) {
+        if (!isbn.matches("\\d{13}")){
+            return false;
+        }
+        int soma = 0;
+        for (int i = 0; i < 13; i++){
+            int peso = (i % 2 == 0) ? 1: 3;
+            soma += (isbn.charAt(i) - '0') * peso;
+        }
+        return soma % 10 == 0;
     }
 }
