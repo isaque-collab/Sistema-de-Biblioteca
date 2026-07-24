7249c52033c2e5a1fe7c7c878c9ad90fbbd096fc
Isaque Cunha <costaisaque716@gmail.com>
2026-07-24 17:54:53 -0300

    feat: criado validador de nomes

--- src/main/java/br/com/biblioteca/validator/NomeValidator.java
+++ src/main/java/br/com/biblioteca/validator/NomeValidator.java
@@ -1,4 +1,26 @@
 package br.com.biblioteca.validator;
 
-public class NomeValidator {
+import br.com.biblioteca.exception.NomeInvalidoException;
+
+public final class NomeValidator {
+
+    private NomeValidator() {
+
+    }
+
+    public static void validar(String nome, int tamanhoMaximo) {
+        if (nome == null) {
+            throw new NomeInvalidoException("O nome não pode ser nulo.");
+        }
+
+        nome = nome.trim();
+
+        if (nome.isEmpty()){
+            throw new NomeInvalidoException("O nome não pode estar vazio.");
+        }
+
+        if (nome.length() > tamanhoMaximo) {
+            throw new NomeInvalidoException("O nome deve ter no máximo "+ tamanhoMaximo + " caracteres.");
+        }
+    }
 }
